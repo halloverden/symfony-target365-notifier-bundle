87c4cbb23c541dbbf33c1e7688b2ec1102bdfec5
Tom Roar Furunes <tom.roar@halloverden.no>
2023-10-25 16:58:40 +0200

    ADD: initial bundle setup

---
+++ src/HalloVerdenTarget365NotifierBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace HalloVerden\Target365NotifierBundle;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class HalloVerdenTarget365NotifierBundle extends AbstractBundle {
+
+}
