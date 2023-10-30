57bac94d5d3594c72f7483b7a0eb50c12db2d69c
Tom Roar Furunes <tom.roar@halloverden.no>
2023-10-30 13:54:41 +0100

    ADD: Target365TransportFactory

---
+++ src/Transport/Target365TransportFactory.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace HalloVerden\Target365NotifierBundle\Transport;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Notifier\Exception\UnsupportedSchemeException;
+use Symfony\Component\Notifier\Transport\AbstractTransportFactory;
+use Symfony\Component\Notifier\Transport\Dsn;
+use Symfony\Component\Notifier\Transport\TransportInterface;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Target365\ApiSdk\Exception\ApiClientException;
+
+final class Target365TransportFactory extends AbstractTransportFactory {
+  public const SCHEME = 'target365';
+
+  public function __construct(
+    EventDispatcherInterface $dispatcher = null,
+    HttpClientInterface $client = null,
+    private readonly ?LoggerInterface $logger = null
+  ) {
+    parent::__construct($dispatcher, $client);
+  }
+
+  /**
+   * @inheritDoc
+   */
+  protected function getSupportedSchemes(): array {
+    return [self::SCHEME];
+  }
+
+  /**
+   * @inheritDoc
+   * @throws ApiClientException
+   */
+  public function create(Dsn $dsn): TransportInterface {
+    $scheme = $dsn->getScheme();
+
+    if (self::SCHEME !== $scheme) {
+      throw new UnsupportedSchemeException($dsn, self::SCHEME, $this->getSupportedSchemes());
+    }
+
+    $keyName = $this->getUser($dsn);
+    $privateKey = $this->getPassword($dsn);
+    $from = $dsn->getOption('from', '');
+    $allowUnicode = $dsn->getOption('allowUnicode') === 'true';
+    $host = 'default' === $dsn->getHost() ? null : $dsn->getHost();
+    $port = $dsn->getPort();
+
+    return new Target365Transport($keyName, $privateKey, $from, $allowUnicode, $host, $port, $this->logger, $this->client, $this->dispatcher);
+  }
+
+}
